8fd385932f3f8fd7a86b743cf5882c5481d247f5
rafay <rafitahmix@gmail.com>
2023-07-29 15:31:26 -0400

    Intial commit

---
+++ functions.py
@@ -0,0 +1,29 @@
+
+FILEPATH = "todos.txt"
+def get_todos(filepath="Todos.txt"):
+    """Read a text file and return the list of to-do items
+    of
+    to - do
+    items"""
+    with open(filepath, 'r') as file_local:
+        todos_local = file_local.readlines()
+    return todos_local
+
+
+def write_todos(todos_arg, filepath='Todos.txt'):
+    with open(filepath,'w') as file:
+        file.writelines(todos_arg)
+    return todos_arg
+
+
+
+
+
+
+
+
+
+
+
+
+
